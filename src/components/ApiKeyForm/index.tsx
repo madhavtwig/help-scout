63800c8516b19d3ad044a8e62edd10af1776a423
Madhav <152593482+madhavtwig@users.noreply.github.com>
2023-12-27 14:42:23 +0000

    Update ux for api key

--- src/components/ApiKeyForm/index.tsx
+++ src/components/ApiKeyForm/index.tsx
@@ -1,6 +1,7 @@
 import { ChangeEvent, MouseEvent, useState } from "react";
 import { setSessionStorage } from "../../utils/localStorage";
 import { LocalStorage } from "../../enum/localStorage";
+import { Button, Heading } from "@helpscout/ui-kit";
 
 type ApiKeyFormType = {
   setTwigApiKey: (value: string) => void;
@@ -17,26 +18,22 @@ export default function ApiKeyForm(props: ApiKeyFormType) {
     <div className="container mx-auto w-screen px-4 py-2">
       <div className="bg-white shadow sm:rounded-lg">
         <div className="px-4 py-5 sm:p-6">
-          <h3 className="text-lg leading-6 font-medium text-gray-900">
+          <Heading level="h1" state="default">
             Set Twig API Key
-          </h3>
-
+          </Heading>
           <div className="mt-5 sm:flex sm:items-center">
-            <div className="flex flex-col  w-full">
+            <div className="flex flex-col space-y-2 w-full">
               <div className="w-full">
                 <input
                   onChange={handleChange}
-                  className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                  className="p-2 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
                   placeholder="Twig API key"
                   value={props.twigApiKey}
                 />
               </div>
-              <button
-                className="mt-3 w-full inline-flex items-center justify-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-                onClick={props.onSave}
-              >
+              <Button className="mt-3 w-full" onClick={props.onSave}>
                 Save
-              </button>
+              </Button>
             </div>
           </div>
         </div>
